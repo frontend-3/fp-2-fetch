76fbaaf0d193b428271c79496c9ca194a63417d7
Elizabeth Manrique <ecmanrique@gmail.com>
2017-10-24 08:15:22 -0500

    Adding server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app     = express();
+const path    = require("path");
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
